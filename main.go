0ed911ee8e33505182bcac0140ca484da8423dcb
Денислан Кусемисов <d.kusemisov@gisgis.local>
2024-12-11 17:33:52 +0300

    added dynamic work with reglaments and dictionaries

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"strings"
 )
 
 type RejectionReasonStruct struct {
@@ -14,111 +16,31 @@ type RejectionReasonStruct struct {
 }
 
 type CriterionStruct struct {
-	//XMLName         xml.Name `xml:"criterion"`
 	Name            string `xml:"name"`
 	NegativeMeaning string `xml:"negativeMeaning"`
 }
 
-func main() {
-	service := "RS"
-
-	reglament, err := os.Open("./reglaments/" + service + ".xml")
+func openDictionary(dictionary, service string) (*os.File, error) {
+	specific_path := fmt.Sprintf("./dictionaries/%s_%s.xml", service, dictionary)
+	general_path := fmt.Sprintf("./dictionaries/%s.xml", dictionary)
+	dict, err := os.Open(specific_path)
 	if err != nil {
-		log.Fatalf("reglament opening failure: %v", err)
-	}
-	defer reglament.Close()
-
-	decoder := xml.NewDecoder(reglament)
-
-	rejections := make(map[string]string, 0)
-	negMeanings := make(map[CriterionStruct]string, 0)
-
-	var rejectionReason string
-	var crit CriterionStruct
-
-	for {
-		token, err := decoder.Token()
-		if err != nil {
-			break
+		if pathErr, ok := err.(*fs.PathError); ok {
+			// add output here that wasn't found specific dict for this service and used general one
+			fmt.Printf("%s (path \"%s\")\n", pathErr.Err, pathErr.Path)
+			dict, err = os.Open(general_path)
+			if err != nil {
+				return nil, fmt.Errorf("%s.xml dictionary opening failure: %v", dictionary, err)
+			}
+		} else {
+			return nil, fmt.Errorf("%s_%s.xml dictionary opening failure: %v", service, dictionary, err)
 		}
-
-		switch t := token.(type) {
-		case xml.StartElement:
-			switch t.Name.Local {
-			case "criterion":
-				err = decoder.DecodeElement(&crit, &t)
-				if err != nil {
-					log.Fatalf("criterion parsing failure: %v", err)
-				}
-				//fmt.Printf("%v\n", crit)
-			case "rejectionReason":
-				var rej RejectionReasonStruct
-				err = decoder.DecodeElement(&rej, &t)
-				if err != nil {
-					log.Fatalf("rejectionReason parsing failure: %v", err)
-				}
-				rejectionReason = rej.Name
-				//fmt.Printf("%v\n", criteria)
-			}
-		case xml.EndElement:
-			if t.Name.Local == "criteria" {
-				//fmt.Printf("%s ------ %s\n", crit.Name, crit.NegativeMeaning)
-
-				if rejectionReason != "" {
-					if _, ok := rejections[rejectionReason]; !ok {
-						rejections[rejectionReason] = "TRASH"
-					}
-				} else if crit.NegativeMeaning != "" {
-					//fmt.Printf("%s ------ %s\n", crit.Name, crit.NegativeMeaning)
-					if _, ok := negMeanings[crit]; !ok {
-						negMeanings[crit] = "TRASH"
-					}
-				}
-
-				rejectionReason = ""
-				crit = CriterionStruct{}
-			}
-		}
-		/*
-			switch t := token.(type) {
-			case xml.StartElement:
-				if t.Name.Local == "rejectionReason" {
-					exit := false
-					for !exit {
-						token2, err := decoder.Token()
-						if err != nil {
-							break
-						}
-						switch t2 := token2.(type) {
-						case xml.StartElement:
-							if t2.Name.Local != "id" {
-								exit = true
-							}
-
-						case xml.CharData:
-							if str_t := strings.TrimSpace(string(t2)); str_t != "" {
-								rejections[str_t] = "TRASH"
-								exit = true
-							}
-						}
-					}
-				}
-			}
-		*/
 	}
+	return dict, nil
+}
 
-	var rejectReasonDict *os.File
-	if service == "RS" {
-		rejectReasonDict, err = os.Open("./dictionaries/rejectReasonForRS.xml")
-	} else {
-		rejectReasonDict, err = os.Open("./dictionaries/rejectReason.xml")
-	}
-	if err != nil {
-		log.Fatalf("rejectReason dictionary opening failure: %v", err)
-	}
-	defer rejectReasonDict.Close()
-
-	decoder = xml.NewDecoder(rejectReasonDict)
+func decodeRejectReasonsFromDict(dict *os.File, rejections map[string]string) error {
+	decoder := xml.NewDecoder(dict)
 	for {
 		token, err := decoder.Token()
 		if err != nil {
@@ -135,10 +57,10 @@ func main() {
 						//fmt.Printf("elem: %s\n", name)
 
 						var data map[string]interface{}
-
 						err := json.Unmarshal([]byte(name), &data)
 						if err != nil {
-							log.Fatalf("json parsing failure: %v", err)
+							// return to output file info about that
+							return fmt.Errorf("json parsing failure of %s: %v", dict.Name(), err)
 						}
 
 						if data["name"] == nil {
@@ -148,57 +70,17 @@ func main() {
 						if _, ok := rejections[data["name"].(string)]; ok {
 							rejections[data["name"].(string)] = attr.Value
 							fmt.Printf("found reason record %s\n", attr.Value)
-							/*
-								exit := false
-								for !exit {
-									token2, err := decoder.Token()
-									if err != nil {
-										break
-									}
-									switch t2 := token2.(type) {
-									case xml.CharData:
-										//fmt.Printf("attrs ---- %s\n", strings.TrimSpace(string(t2)))
-										if str_t := strings.TrimSpace(string(t2)); str_t != "" {
-											//fmt.Printf("attrs ---- %s\n", str_t)
-
-											var data map[string]interface{}
-
-											err := json.Unmarshal([]byte(str_t), &data)
-											if err != nil {
-												log.Fatalf("json parsing failure: %v", err)
-											}
-
-											rejections[attr.Value] = data["name"].(string)
-											exit = true
-										}
-									}
-								}
-							*/
 						}
 					}
 				}
 			}
 		}
 	}
+	return nil
+}
 
-	var negativeMeaningDict *os.File
-	if service == "RS" {
-		negativeMeaningDict, err = os.Open("./dictionaries/reasonForSuccessDecisionForRS.xml")
-	} else {
-		negativeMeaningDict, err = os.Open("./dictionaries/reasonForSuccessDecision.xml")
-	}
-	if err != nil {
-		log.Fatalf("reasonForSuccessDecision dictionary opening failure: %v", err)
-	}
-	defer negativeMeaningDict.Close()
-
-	temp, err := os.Create("temp")
-	if err != nil {
-		log.Fatalf("output creation failure: %v", err)
-	}
-	defer temp.Close()
-
-	decoder = xml.NewDecoder(negativeMeaningDict)
+func decodeNegativeMeaningsFromDict(dict *os.File, negMeanings map[CriterionStruct]string) error {
+	decoder := xml.NewDecoder(dict)
 	for {
 		token, err := decoder.Token()
 		if err != nil {
@@ -215,10 +97,10 @@ func main() {
 						//fmt.Printf("elem: %s\n", name)
 
 						var data map[string]interface{}
-
 						err := json.Unmarshal([]byte(jsonStr), &data)
 						if err != nil {
-							log.Fatalf("json parsing failure: %v", err)
+							// return to output file info about that
+							return fmt.Errorf("json parsing failure of %s: %v", dict.Name(), err)
 						}
 
 						if data["negativeMeaning"] == nil || data["name"] == nil {
@@ -230,88 +112,166 @@ func main() {
 						if _, ok := negMeanings[obj]; ok {
 							negMeanings[obj] = attr.Value
 							//fmt.Printf("found negMeaning record %s for '%.190s'\n", attr.Value, data["negativeMeaning"].(string))
-							_, err = temp.Write([]byte(attr.Value + "   " + obj.NegativeMeaning + " -- " + obj.Name + "\n"))
-							if err != nil {
-								log.Fatalf("writing temp_file failure: %v", err)
-							}
-
-							/*
-								exit := false
-								for !exit {
-									token2, err := decoder.Token()
-									if err != nil {
-										break
-									}
-									switch t2 := token2.(type) {
-									case xml.CharData:
-										//fmt.Printf("attrs ---- %s\n", strings.TrimSpace(string(t2)))
-										if str_t := strings.TrimSpace(string(t2)); str_t != "" {
-											//fmt.Printf("attrs ---- %s\n", str_t)
-
-											var data map[string]interface{}
-
-											err := json.Unmarshal([]byte(str_t), &data)
-											if err != nil {
-												log.Fatalf("json parsing failure: %v", err)
-											}
-
-											rejections[attr.Value] = data["name"].(string)
-											exit = true
-										}
-									}
-								}
-							*/
 						}
 					}
 				}
 			}
 		}
 	}
+	return nil
+}
 
-	/*n := 1
-	for item := range negMeanings {
-		fmt.Printf("%d. %d %s\n", n, len([]rune(item)), item)
-		n++
-	}*/
+func main() {
+
+	fileSystem := os.DirFS(".")
 
 	text := `do
 $$
 declare
-    rec record;
-    key_type int8;
+	rec record;
+	key_type int8;
 begin
-    for rec in (select scheme from regadm.m_projects where scheme = 'kostgo') loop
-        perform set_config('search_path', rec.scheme, true);
-        raise info '%', rec.scheme;
+	for rec in (select scheme from regadm.m_projects where scheme = 'kostgo') loop
+		perform set_config('search_path', rec.scheme, true);
+		raise info '%', rec.scheme;
 
-        select key into key_type from kostgo.d_ref_dependent_type where full_name like 'DocRefRejectReasonType';
-        raise info 'key of DocRefRejectReasonType = %', key_type;
+		select key into key_type from d_ref_dependent_type where full_name like 'DocRefRejectReasonType';
+		raise info 'key of DocRefRejectReasonType = %', key_type;
 `
 
-	//fmt.Println(text)
-
-	i := 0
-	for rejectName, rejectId := range rejections {
-		i++
-		text += fmt.Sprintf("        -- %d characters in name\n        ", len([]rune(rejectName)))
-
-		if rejectId == "TRASH" {
-			text += "--"
+	fs.WalkDir(fileSystem, "reglaments", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
 		}
-		text += fmt.Sprintf("insert into d_ref_dependents (alias, code_kcr, dependent_type, full_name, name, is_draft, sys_status) values ('Opal%sRejReason%d', '%s', key_type, '%s','%s', 0, 0);\n",
-			service, i, rejectId, rejectName, rejectName)
-	}
-	text += "\n"
-	for obj, negMeanId := range negMeanings {
-		i++
-		text += fmt.Sprintf("        -- %d characters in name\n        ", len([]rune(obj.NegativeMeaning)))
-
-		if negMeanId == "TRASH" {
-			text += fmt.Sprintf("--name in dictionary: %s\n        --", obj.Name)
+		if d.IsDir() {
+			return nil
 		}
-		text += fmt.Sprintf("insert into d_ref_dependents (alias, code_kcr, dependent_type, full_name, name, is_draft, sys_status) values ('Opal%sRejMeaning%d', '%s', key_type, '%s','%s', 0, 0);\n",
-			service, i, negMeanId, obj.NegativeMeaning, obj.NegativeMeaning)
-	}
+		fmt.Println(path)
+
+		service := strings.Split(d.Name(), ".")[0]
+		fmt.Println(service)
+
+		reglament, err := os.Open(path)
+		if err != nil {
+			log.Fatalf("reglament opening failure: %v", err)
+		}
+		defer reglament.Close()
+
+		decoder := xml.NewDecoder(reglament)
+
+		rejections := make(map[string]string)
+		negMeanings := make(map[CriterionStruct]string)
+
+		var rejectionReason string
+		var crit CriterionStruct
+
+		for {
+			token, err := decoder.Token()
+			if err != nil {
+				break
+			}
+
+			switch t := token.(type) {
+			case xml.StartElement:
+				switch t.Name.Local {
+				case "criterion":
+					err = decoder.DecodeElement(&crit, &t)
+					if err != nil {
+						log.Fatalf("criterion parsing failure: %v", err)
+					}
+					//fmt.Printf("%v\n", crit)
+				case "rejectionReason":
+					var rej RejectionReasonStruct
+					err = decoder.DecodeElement(&rej, &t)
+					if err != nil {
+						log.Fatalf("rejectionReason parsing failure: %v", err)
+					}
+					rejectionReason = rej.Name
+					//fmt.Printf("%v\n", criteria)
+				}
+			case xml.EndElement:
+				if t.Name.Local == "criteria" {
+					//fmt.Printf("%s ------ %s\n", crit.Name, crit.NegativeMeaning)
+					if rejectionReason != "" {
+						if _, ok := rejections[rejectionReason]; !ok {
+							rejections[rejectionReason] = "TRASH"
+						}
+					} else if crit.NegativeMeaning != "" {
+						//fmt.Printf("%s ------ %s\n", crit.Name, crit.NegativeMeaning)
+						if _, ok := negMeanings[crit]; !ok {
+							negMeanings[crit] = "TRASH"
+						}
+					}
+
+					rejectionReason = ""
+					crit = CriterionStruct{}
+				}
+			}
+		}
+
+		rejectReasonDict, err := openDictionary("rejectReason", service)
+		if err != nil {
+			// add to output file
+			log.Printf("couldn't open rejectReason dictionary: %v\n", err)
+		} else {
+			defer rejectReasonDict.Close()
+			err = decodeRejectReasonsFromDict(rejectReasonDict, rejections)
+			if err != nil {
+				//write to output file error message
+			}
+		}
+
+		negativeMeaningDict, err := openDictionary("reasonForSuccessDecision", service)
+		if err != nil {
+			// add to output file
+			log.Printf("couldn't open reasonForSuccessDecision dictionary: %v\n", err)
+		} else {
+			defer negativeMeaningDict.Close()
+			err = decodeNegativeMeaningsFromDict(negativeMeaningDict, negMeanings)
+			if err != nil {
+				//write to output file error message
+			}
+		}
+
+		i := 0
+		if len(rejections) != 0 {
+			for rejectName, rejectId := range rejections {
+				i++
+				if len([]rune(rejectName)) >= 1000 {
+					text += fmt.Sprintf("          -- %d characters in full_name, could be limit in db on that column in 1000 symbols\n        --", len([]rune(rejectName)))
+				}
+
+				if !strings.HasSuffix(text, "--") {
+					text += "        "
+					if rejectId == "TRASH" {
+						text += "  --couldn't find myself, so you need to do that using name dictionary:\n        --"
+					}
+				}
+				text += fmt.Sprintf("insert into d_ref_dependents (alias, code_kcr, dependent_type, full_name, name, is_draft, sys_status) values ('Opal%sRejReason%d', '%s', key_type, '%s','%s', 0, 0);\n",
+					service, i, rejectId, rejectName, rejectName)
+			}
+			text += "\n"
+		}
+		if len(negMeanings) != 0 {
+			for obj, negMeanId := range negMeanings {
+				i++
+				if len([]rune(obj.NegativeMeaning)) >= 1000 {
+					text += fmt.Sprintf("          -- %d characters in full_name, could be limit in db on that column in 1000 symbols\n        ", len([]rune(obj.NegativeMeaning)))
+				}
+
+				if !strings.HasSuffix(text, "--") {
+					text += "        "
+					if negMeanId == "TRASH" {
+						text += "  --couldn't find myself, so you need to do that. name in dictionary:\n        --"
+					}
+				}
+				text += fmt.Sprintf("insert into d_ref_dependents (alias, code_kcr, dependent_type, full_name, name, is_draft, sys_status) values ('Opal%sRejMeaning%d', '%s', key_type, '%s','%s', 0, 0);\n",
+					service, i, negMeanId, obj.NegativeMeaning, obj.NegativeMeaning)
+			}
+		}
+
+		return nil
+	})
 
 	text += `    end loop;
 end;
@@ -327,15 +287,6 @@ $$;`
 		log.Fatalf("writing output failure: %v", err)
 	}
 	//fmt.Println(text)
-
-	//fmt.Println(len([]rune("Заключение, подтверждающие соблюдение требований технических регламентов при размещении планируемого к строительству или реконструкции объекта капитального строительства.pdf")))
-	/*
-	   	fmt.Println(len("наличие у уполномоченных на выдачу разрешений на строительство федерального органа исполнительной власти, органа исполнительной власти субъекта Российской Федерации,
-	       органа местного самоуправления, государственной корпорации по атомной энергии Росатом или государственной корпорации по космической деятельности Роскосмос информации о выявленном в
-	       рамках государственного строительного надзора, государственного земельного надзора или муниципального земельного контроля факте отсутствия начатых работ по строительству, реконструкции
-	       на день подачи заявления о внесении изменений в разрешение на строительство в связи с продлением срока действия такого разрешения или информации органа государственного строительного
-	       надзора об отсутствии извещения о начале данных работ, если направление такого извещения является обязательным в соответствии с требованиями части 5 статьи 52 Градостроительного кодекса Российской Федерации, в случае, если внесение изменений в разрешение на строительство связано с продлением срока действия разрешения на строительство"))
-	*/
 }
 
 /*
@@ -358,57 +309,4 @@ $$;`
 <tns:rejectionReason>
 <tns:id>
 chardate here
-
-VS01187003GGER03_3T
-["VS01187003GGER03_3T"]
-
-18:30:35.393 INFO 100,00% Выполнение миграции данных
-18:30:36.233 INFO 100,00% 00:00:00.8403659 Миграция данных - пропущено
-
-log_path="$dir_path/$schema_name.log"
-last_string_num=$(wc -l $log_path | sed s!$log_path!!)
-finish_line=$(sed -n "${last_string_num}p" $log_path)
-
-
-Ошибка передачи элемента: "{"ID":"d9c77f45-f856-422f-bcf7-6214112656ff","Denomination":null,"State":null,"Type":null,"Arrangement":null,"Location":null,"CadastralNumber":null,"Len
- System.AggregateException: One or more errors occurred. (Ошибка отправки XML пакета: "{
-  "errorId": "00-258b1a254d910b7cd132e8349d35c98a-2f607b2d7c3bf217-00",
-  "message": "Some kind of error happened in the API. Errors: The element \u0027Fields\u0027 has incomplete content. List of possible elements expected: \u0027Source\u0027."
-}")
-
-
-
-
-trigger_word='FATAL'
-log_path="ekbgo.log"
-last_lines=$(tail -n 50 "$log_path")
-
-if [[ "$last_lines" != *"$trigger_word"* ]];
-then
-    echo "Метаданные ЗАЛИТЫ"
-else
-    echo "НЕ УДАЛОСЬ залить метаданные"
-fi
-
-
-
-"0af40b04-7295-1661-8172-c1ee57272d0f",
-
-"INTERAGENCY_REQUEST_SENDING": [
-    "0ae94758-7be3-1c2b-817b-f23748c80027",
-    "0ae96485-8abd-1f11-818b-8b789ad0543a",
-    "0ae947aa-8632-122f-8186-4a08412904aa",
-    "0ae95269-921d-1155-8192-1f4472220142",
-    "a50b8a3c-b546-11ea-b804-03b6cb4f5441",
-    "a50b6336-b546-11ea-b803-2394ab708be7",
-        "0ae95273-915a-1ad8-8191-7eed50020602",
-        "0ae947aa-8632-122f-8186-4a08412a04ab",
-        "0ae9644c-87a3-197a-8187-b2eb1dfb00f6",
-        "0ae947f5-89b6-15bc-8189-ba5cf8ce0064",
-        "0ae9526d-8abd-1e6c-818b-ccd2bdf84acb",
-        "0ae95269-921d-1155-8192-9b2707b71c7a",
-        "0ae9645e-9027-1bf5-8190-3a3c139b04cd",
-        "0ae95269-921d-1155-8192-1f4472220142"
-
-  ],
 */
